190001c9b88c0b26464d786ed49819864da9278d
jeremy <jeremyeparker@yahoo.com>
2022-11-29 16:16:25 -0500

    so much added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { Box } from '@mantine/core';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Box, Button, List, ThemeIcon } from '@mantine/core';
 import useSWR from 'swr';
+import AddToDo from './components/AddToDo';
+import { CheckCircleFillIcon } from '@primer/octicons-react';
 
-export const ENDPOINT = "http://localhost:4000"
+export interface Todo{
+  id:number;
+  title:string;
+  body:string;
+  done:boolean;
+
+}
+
+export const ENDPOINT = "http://localhost:4000";
 
 const fetcher = (url: string) =>
  fetch(`${ENDPOINT}/${url}`).then((r)=> r.json());
 
 function App() {
 
-  const {data, mutate } = useSWR('api/todos', fetcher)
+  const {data, mutate } = useSWR<Todo[]>('api/todos', fetcher);
 
-  return <Box>{JSON.stringify(data)}</Box>; 
+  async function markToDoAndDone(id: number){
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`,{
+      method:"PATCH"
+    }).then((r) => r.json());
+
+    mutate(updated)
+  }
+
+  return (
+    <Box sx={(theme)=>({
+        padding: "2rem",
+        width:"100%",
+        maxWidth:"40rem",
+        margin: "0 auto",
+        background:"white"
+      })}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) =>{
+          return <List.Item onClick={() => markToDoAndDone(todo.id)} key={`todo__${todo.id}`} 
+
+            icon={
+              todo.done ? (
+              <ThemeIcon color="lightpink" size={24} radius="xl">
+                <CheckCircleFillIcon size={20}/>
+              </ThemeIcon>
+              ):(
+              <ThemeIcon color="gray" size={24} radius="xl">
+                <CheckCircleFillIcon size={20}/>
+              </ThemeIcon>
+              )            
+            }
+
+            >{todo.title}
+          </List.Item>
+        })}
+      </List>
+      <AddToDo mutate = {mutate}/>
+    </Box>  
+  );
 }
 
-export default App
+export default App;
